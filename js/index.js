6f3b1f29d423162461bd8c3839f819ff263bb5b4
nicoKnecht <nicosanmarco.dev@gmail.com>
2023-10-30 00:39:09 -0300

    improve js done until now

--- js/index.js
+++ js/index.js
@@ -7,22 +7,38 @@ const menuMobile = document.querySelector('.navMenuWrapper')
 const faqsToggle = document.querySelectorAll('.faqsSubtitle');//aray
 let faqsInfo = document.querySelectorAll('.faqsInformation');// array
 
-/*Evento para menu  */
-menuToggle.addEventListener('click', () => {
+//Localstorage 
+
+//login&register
+
+
+/*FUNCIONES */
+
+//FUNCION PARA ABRIR MENU
+const openmenu = () => {
     menuMobile.classList.toggle('open');
 
-});
+};
 
-/*Evento para faqs  */
-faqsToggle.forEach(item => {// como faqsToggle e sun array, debo aplicar el evnto a c/u con un bucle
-    item.addEventListener('click', (e) => {
-        console.log(e.target.id);
-        let j = e.target.id; // capturo id del elemento clickeado
-        for (let i = 0; i < faqsInfo.length; i++) {// comparo con el array que contiene la lista de clasess faqsInfo
-            if (i == j) {
-                faqsInfo[i].classList.toggle('open');
-            }
+
+const openFaqs = (e) => {
+    console.log(e.target.id);
+    let j = e.target.id; // captura el ID del elemento clickeado
+    for (let i = 0; i < faqsInfo.length; i++) {// compara con el array que contiene la lista de clases faqsInfo
+        if (i == j) {
+            faqsInfo[i].classList.toggle('open');
         }
+    }
+}
 
-    })
-});
+
+
+const init = () => {
+
+    menuToggle.addEventListener('click', openmenu);
+    faqsToggle.forEach(item => {
+        item.addEventListener('click', openFaqs);
+    });
+}
+
+init();
